d06be5904a3a345fce2d4eca1ed74c6593e31ec1
opensource-a <62256831+opensource-a@users.noreply.github.com>
2021-10-08 20:04:17 +0530

    Update setup-pipeline.sh

--- pac/setup-pipeline.sh
+++ pac/setup-pipeline.sh
@@ -9,4 +9,4 @@ aws s3 cp . s3://$s3Bucket/$pipelineFilesPath/ --recursive
 aws cloudformation create-stack --stack-name $stackName --template-url https://s3.amazonaws.com/$s3Bucket/$pipelineFilesPath/cloudformation.yaml --parameters file://parameters.json --capabilities CAPABILITY_NAMED_IAM
 codepipelineRoleArn=$(aws cloudformation describe-stacks --stack-name $stackName --query "Stacks[0].Outputs[?OutputKey=='CodePipelineRoleArn'].OutputValue" --output text)
 
-aws kms create-grant --key-id arn:aws:kms:us-east-1:367644944967:key/mrk-0c3e558cf35f4c78affd6c71d9336604 --operations Decrypt Encrypt GenerateDataKey --grantee-principal $codepipelineRoleArn --name $stackName-grant
+aws kms create-grant --key-id $secretsKeyArn --operations Decrypt Encrypt GenerateDataKey --grantee-principal $codepipelineRoleArn --name $stackName-grant
