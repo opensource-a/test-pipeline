d7ef169175eb39597bbc95a704f00d0f8f6f60ef
opensource-a <62256831+opensource-a@users.noreply.github.com>
2021-10-11 21:19:35 +0530

    Update update-pipeline.sh

--- pac/update-pipeline.sh
+++ pac/update-pipeline.sh
@@ -7,3 +7,4 @@ done < <(sed -nE 's/([a-z_0-9]+): (.*)/\1=\2/ p' pipeline-parameters.yaml)
 
 aws s3 cp . s3://$s3Bucket/$pipelineFilesPath/ --recursive
 aws cloudformation update-stack --stack-name $stackName --template-url https://s3.amazonaws.com/$s3Bucket/$pipelineFilesPath/cloudformation.yaml --parameters file://parameters.json --capabilities CAPABILITY_NAMED_IAM CAPABILITY_AUTO_EXPAND
+aws s3 rm s3://$s3Bucket/$pipelineFilesPath
